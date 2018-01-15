7ca39fceea583225e95acb191f11b824ada197a2
dbroeder <dbroeder@gmail.com>
2018-01-14 22:03:34 -0600

    lots of cool changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { RemovePlayerPage} from '../pages/remove-player/remove-player'
+import { RoundsPage} from '../pages/rounds/rounds';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    RemovePlayerPage
+    RemovePlayerPage,
+    RoundsPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ContactPage,
     HomePage,
     TabsPage,
-    RemovePlayerPage
+    RemovePlayerPage,
+    RoundsPage
   ],
   providers: [
     StatusBar,
--- src/pages/remove-player/remove-player.ts
+++ src/pages/remove-player/remove-player.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { AlertController,IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the RemovePlayerPage page.
@@ -18,7 +18,7 @@ export class RemovePlayerPage {
   playerNum:number;
   totalNumofPlayers:number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
+  constructor(public alertCtrl: AlertController ,public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   }
 
   ionViewDidLoad() {
@@ -33,6 +33,13 @@ export class RemovePlayerPage {
   removePlayer(){
     if(this.playerNum!==undefined&&this.playerNum>=1&&this.playerNum<=this.totalNumofPlayers){
       this.viewCtrl.dismiss(this.playerNum);
+    }else{
+      let alert=this.alertCtrl.create({
+        title:'Try Again',
+        subTitle:'That player does not exist. Enter a player number between 1 and '+this.totalNumofPlayers,
+        buttons:['ok']
+      });
+      alert.present();
     }
     
   }
