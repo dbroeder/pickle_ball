e92cb2066135a4a6af864a2261c5d9cc52a89395
dbroeder <dbroeder@4rhuskies.org>
2018-03-03 15:07:24 -0600

    Update select-player.ts

--- src/pages/select-player/select-player.ts
+++ src/pages/select-player/select-player.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the SelectPlayerPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+
 
 @Component({
   selector: 'page-select-player',
